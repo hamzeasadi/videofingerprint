872048221be704aecc8645119be385d91d8d9411
hamzeasadi <hamzeasadi1@gmail.com>
2023-01-26 18:57:47 +0000

    change loss func

--- src/engine.py
+++ src/engine.py
@@ -14,18 +14,18 @@ def train_step(model: nn.Module, data: DataLoader, criterion: nn.Module, optimiz
     epoch_error = 0
     l = len(data)
     model.train()
-    with torch.autograd.detect_anomaly(True):
-        for i, (X1, X2) in enumerate(data):
-            X1 = X1.to(dev).squeeze()
-            X2 = X2.to(dev).squeeze()
-            out1, out2 = model(X1, X2)
-            # out = torch.cat((out1, out2), dim=0).squeeze()
-            loss = criterion(out1, out2)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            epoch_error += loss.item()
-            # break
+    # with torch.autograd.detect_anomaly(True):
+    for i, (X1, X2) in enumerate(data):
+        X1 = X1.to(dev).squeeze()
+        X2 = X2.to(dev).squeeze()
+        out1, out2 = model(X1, X2)
+        # out = torch.cat((out1, out2), dim=0).squeeze()
+        loss = criterion(out1, out2)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        epoch_error += loss.item()
+        # break
     return epoch_error/l
 
 
